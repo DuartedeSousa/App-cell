7ca0ce044c253f974ca32a2a276517e5c520e526
Duarte <duarte.desousa.contato@gmail.com>
2026-09-21 15:50:24 -0300

    fourth commit

---
+++ app-pra-cll-main/GameHub/src/data/jogos.js
@@ -0,0 +1,37 @@
+//--------------------------------------
+    //ETAPA 2 - centralizar cores
+    //--------------------------------------
+
+    export const jogos = [
+        {
+            id: "1",
+            nome: "Minecraft",
+            genero: "Sandbox",
+            nota: 4.8,
+            plataforma: "PC, Console, Mobile",
+            Descrição: "Um mundo feito de blocos onde você pode construir",
+            imagem: require("../../assets/games/minecraft.jpg"),
+            destaque: true,
+        },
+        
+        {
+            id: "7",
+            nome: "The Sims",
+            genero: "Simulação",
+            nota: 4.2,
+            plataforma: "PC, Console, Mobile",
+            Descrição: "Simulador de vida real",
+            imagem: require("../../assets/games/the-sims.jpg"),
+            destaque: false,
+        },
+        {
+            id: "8",
+            nome: "Among US",
+            genero: "Detetive",
+            nota: 4.1,
+            plataforma: "PC, Console, Mobile",
+            Descrição: "Descubra quem é o impostor",
+            imagem: require("../../assets/games/among-us.jpg"),
+            destaque: false,
+        }
+    ]
