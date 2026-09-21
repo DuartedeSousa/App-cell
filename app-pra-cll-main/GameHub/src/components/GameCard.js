113a502dc67460715c9696729b7d3b7491c4bfa3
Duarte <duarte.desousa.contato@gmail.com>
2026-09-21 16:06:52 -0300

    fifth commit

---
+++ app-pra-cll-main/GameHub/src/components/GameCard.js
@@ -0,0 +1,31 @@
+//--------------------------------------
+//ETAPA 4 - CRIAR COMPONENTE
+//--------------------------------------
+// Nós vamos reutilizar o componente em 03 telas diferentes (Inicio, Jogos, Favoritos)
+import {view, text, Image, Pressable, StyleSheet} from "react-native";
+import{ useRouter } from "expo-router";
+//navegação programatica.
+import { cores } from "../data/tema";
+
+export default function GameCard ((jogo)){
+    const router = useRouter();
+
+    return (
+        <Pressable
+        style={StyleSheet.card}
+        onPress={() => router.push(`/jogos/${jogo.id}`)} // Navega entre as costas
+        >
+            <Image source={jogo.imagem} style={Styles.imagem}/>
+            <View style={styles.info}>
+                <text style={styles.nome} numbersOfLines={1}>
+                    {jogo.nome}
+                </text>
+                <text style={styles.genero}>{jogo.genero}</text>
+                <text style={styles.nota}>⭐{jogo.nota}</text>
+            </View>
+        </Pressable>
+    )
+}
+const styles = StylesSheet.create({
+    
+})
