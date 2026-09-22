c093a0c9a653a6ac415a8e4aa6b40b163590e130
Duarte <duarte.desousa.contato@gmail.com>
2026-09-22 08:38:15 -0300

    7 commit

--- app-pra-cll-main/GameHub/src/components/GameCard.js
+++ app-pra-cll-main/GameHub/src/components/GameCard.js
@@ -27,5 +27,36 @@ export default function GameCard ({jogo}){
     )
 }
 const styles = StyleSheet.create({
-
+    card: {
+        backGroundColor: cores.fundoCard,
+        borderRadius: 12,
+        overflow: "hidden",
+        width: 158,
+        marginRight: 12,
+        borderWidth: 1,
+        borderColor: cores.borda,
+    },
+    imagem: {
+        width: "100%",
+        height: 110,
+    },
+    info: {
+        padding: 10,
+    },
+    nome: {
+        color: cores.textoPrincipal,
+        fontSize: 14,
+        fontWeight: "bold",
+    },
+    genero: {
+        color: cores.textoPrincipal,
+        fontSize: 12,
+        margnTop: 2,
+    },
+    nota: {
+        color: cores.verde,
+        fontSize: 12,
+        marginTop: 4,
+        fontWeight: 600,
+    }
 })
